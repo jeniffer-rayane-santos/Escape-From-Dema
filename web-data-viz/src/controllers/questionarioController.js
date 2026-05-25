765da5b7aa833d4113c8ff6a57f55271f4547a5c
jeniffer-rayane-santos <jeniffer.santos@sptech.school>
2026-05-25 02:26:07 -0300

    Controller questionario

--- web-data-viz/src/controllers/questionarioController.js
+++ web-data-viz/src/controllers/questionarioController.js
@@ -1,22 +1,16 @@
 var questionarioModel = require("../models/questionarioModel")
 
-
 function salvarQuestao(req, res) {
-
-
     let numPergunta = req.body.numeroPergunta
     let letraQuestao = req.body.questao
     let tipo = req.body.tipoPergunta
     let usuario = req.body.idUsuario
 
-    // trocar mensagem
     if (numPergunta == undefined) {
         res.status(400).send("Seu numero da pergunta está vazio!")
     } else {
-
         questionarioModel.salvarQuestao(numPergunta, letraQuestao, tipo, usuario)
-            .then(
-                function (resultado) {
+            .then(function (resultado) {
                 res.json({
                     mensagem: "Questão cadastrada com sucesso!",
                     dados: {
@@ -26,28 +20,43 @@ function salvarQuestao(req, res) {
                         idUsuario: usuario
                     }
                 });
-            }
-            ).catch(
-                function (erro) {
-                    console.log(erro)
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro da questao! Erro: ",
-                        erro.sqlMessage
-                    )
-                    res.status(500).json(erro.sqlMessage)
-                }
-            )
+            })
+            .catch(function (erro) {
+                console.log(erro)
+                res.status(500).json(erro.sqlMessage)
+            })
     }
 }
 
-function deletarQuestoes(req, res) {
+function obterEstatisticas(req, res) {
+    questionarioModel.obterEstatisticas()
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
 
+function usuarioJogou(req, res) {
+    var idUsuario = req.params.idUsuario;
+    questionarioModel.usuarioJogou(idUsuario)
+        .then(function(resultado) {
+            res.json({ jogou: parseInt(resultado[0].total) > 0 });
+        })
+        .catch(function(erro) {
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
+function deletarQuestoes(req, res) {
     let idUsuario = req.body.idUsuario;
 
     if (idUsuario == undefined) {
         res.status(400).send("O ID do usuário está vazio!");
     } else {
-
         questionarioModel.deletarQuestoes(idUsuario)
             .then(function (resultado) {
                 res.json({
@@ -57,13 +66,39 @@ function deletarQuestoes(req, res) {
             })
             .catch(function (erro) {
                 console.log(erro);
-                console.log("\nHouve um erro ao deletar as questões! Erro: ", erro.sqlMessage);
                 res.status(500).json(erro.sqlMessage);
             });
     }
 }
 
+function obterEscolhasPorPergunta(req, res) {
+    questionarioModel.obterEscolhasPorPergunta()
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
+function obterRespostasJogador(req, res) {
+    var idUsuario = req.params.idUsuario;
+    questionarioModel.obterRespostasJogador(idUsuario)
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
 module.exports = {
     salvarQuestao,
-    deletarQuestoes
+    obterEstatisticas,
+    usuarioJogou,
+    deletarQuestoes,
+    obterEscolhasPorPergunta,
+    obterRespostasJogador
 }
